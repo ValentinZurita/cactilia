ebdd5322dd478d3f8b16157adae0f07430922a3f
ValentinZurita <67253631+ValentinZurita@users.noreply.github.com>
2025-03-23 13:13:08 -0600

    fix: adding checkout route

--- src/modules/shop/router/ShopRoutes.jsx
+++ src/modules/shop/router/ShopRoutes.jsx
@@ -1,13 +1,15 @@
 // src/modules/shop/router/ShopRoutes.jsx
 
 import { Routes, Route } from "react-router-dom";
-import { ShopPage, OrderSuccessPage } from "../pages";
+import { ShopPage, OrderSuccessPage, CheckoutPage } from '../pages'
 
 export const ShopRoutes = () => {
   return (
     <Routes>
       {/** /shop -> ShopPage */}
       <Route index element={<ShopPage />} />
+
+      <Route path="checkout" element={<CheckoutPage />} />
 
       {/** /shop/order-success/:orderId */}
       <Route path="order-success/:orderId" element={<OrderSuccessPage />} />
