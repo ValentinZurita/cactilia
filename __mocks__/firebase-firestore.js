6add85293f209786da3fa5c0d035f16695f2c2f2
ValentinZurita <67253631+ValentinZurita@users.noreply.github.com>
2025-04-28 22:58:35 -0600

    Delete __mocks__ directory

--- __mocks__/firebase-firestore.js
+++
@@ -1,5 +0,0 @@
-// Mock básico de Firebase Firestore
-export const collection = jest.fn(() => ({}));
-export const getDocs = jest.fn(() => Promise.resolve({ docs: [] }));
-export const doc = jest.fn(() => ({}));
-export const getDoc = jest.fn(() => Promise.resolve({ exists: () => false, data: () => ({}) })); 
